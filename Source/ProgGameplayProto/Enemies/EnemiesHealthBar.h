87a39f96ba7a0bb43810180c177af2911740897f
MaximeVdh7th <maximevdh.student@gmail.com>
2023-11-13 00:28:49 +0100

    added HP bar display (part1)

---
+++ Source/ProgGameplayProto/Enemies/EnemiesHealthBar.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../../../../../../../../Program Files/Epic Games/UE_5.3/Engine/Source/Runtime/UMG/Public/Blueprint/UserWidget.h"
+#include "Blueprint/UserWidget.h"
+#include "EnemiesHealthBar.generated.h"
+
+UCLASS(Abstract)
+class PROGGAMEPLAYPROTO_API UEnemiesHealthBar : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	class UProgressBar* HealthBar;
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* CurrentHealthLabel;
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* MaxHealthLabel;
+
+};
