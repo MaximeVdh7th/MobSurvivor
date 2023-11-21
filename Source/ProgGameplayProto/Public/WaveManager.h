dfc45fe20d1a935f94895554aac0c4d3224eae80
MaximeVdh7th <127596914+MaximeVdh7th@users.noreply.github.com>
2023-11-21 12:29:26 +0100

    Matinée du 21 (peu productive :'()

---
+++ Source/ProgGameplayProto/Public/WaveManager.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Info.h"
+#include "WaveManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROGGAMEPLAYPROTO_API AWaveManager : public AInfo
+{
+	GENERATED_BODY()
+
+
+	
+};
