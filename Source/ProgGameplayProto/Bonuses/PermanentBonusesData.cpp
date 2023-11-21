9988047f4fe0bae81fd24bb13af40f57ddaa041e
mms5004 <105063593+mms5004@users.noreply.github.com>
2023-11-21 12:32:05 +0100

    add data asset for permanent update

---
+++ Source/ProgGameplayProto/Bonuses/PermanentBonusesData.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PermanentBonusesData.h"
+
+#include "ProgGameplayProto/GameUtils.h"
+#include "ProgGameplayProto/ProgGameplayProtoCharacter.h"
+
---
+++ Source/ProgGameplayProto/Bonuses/PermanentBonusesData.h
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PermanentBonusesData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROGGAMEPLAYPROTO_API UPermanentBonusesData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (MultiLine = true))
+		FText Description;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		int NUpgrade;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<int> UpgradeCosts;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<float> HPMax;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<float> HPRegen;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<float> CollectionDistance;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<float> GoldLuck;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<float> XPLuck;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<float> Health;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<float> InvicibilityTime;
+
+	///		DASH		///
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<float> DashDistance;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<float> DashCooldown;
+
+	///		Shield		///
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<float> ShieldAbsortion;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<float> ShieldCooldown;
+
+	///		PipeBomb	///
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<float> BombRadiusDamage;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<float> BombDamage;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<float> BombCooldown;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TArray<bool> BombNotSelfDamage;
+	
+};
